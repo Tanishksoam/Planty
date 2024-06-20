809cb85b7ab210ee60d100cc5b4909fe99679977
Tanishksoam <soamtanishk@gmail.com>
2024-06-20 12:35:14 +0530

    get started section done

--- planty/app/components/GetStarted.tsx
+++ planty/app/components/GetStarted.tsx
@@ -54,88 +54,15 @@ const GetStarted: React.FC = () => {
   return (
     <div className="w-full max-w-[960px] h-auto flex flex-col bg-[#212121] justify-start items-start py-20 px-10 gap-8 my-10 rounded-md">
       <div className="w-full flex justify-start items-center text-5xl font-medium">
-        Personalized Plant Picks
+        Get Started Today
       </div>
-      <div className="flex flex-col gap-4 justify-center items-start">
-        <div
-          onClick={handleToggle}
-          className="w-full flex justify-start gap-4 items-start cursor-pointer"
-        >
-          <TbTriangleFilled
-            size={20}
-            className={`transform ${
-              toggle ? "rotate-180" : "rotate-90"
-            } duration-300`}
-          />
-          <h4>Choose from a wide selection</h4>
+      <div className=" w-full flex gap-4 justify-start items-start">
+        <div className="w-auto rounded-md bg-transparent text-[#dcff50] border-2 border-[#dcff50] font-semibold p-4">
+          Explore Subscriptions
         </div>
-        {inProp && (
-          <div
-            className={`w-full px-2 flex justify-start items-start ${
-              toggle
-                ? "fade-enter fade-enter-active"
-                : "fade-exit fade-exit-active"
-            }`}
-          >
-            <div className="px-6 border-l-[1px] border-[#dfdfdf] font-thin">
-              Our curated plant collection includes low-maintenance options,
-              air-purifying plants, blooming varieties, and more, catering to
-              all styles and preferences.
-            </div>
-          </div>
-        )}
-        <div
-          onClick={handleToggle1}
-          className="w-full flex justify-start gap-4 items-start cursor-pointer"
-        >
-          <TbTriangleFilled
-            size={20}
-            className={`transform ${
-              toggle1 ? "rotate-180" : "rotate-90"
-            } duration-300`}
-          />
-          <h4>Specify your needs</h4>
+        <div className="w-auto rounded-md bg-[#dcff50] text-[#212121] font-semibold p-4">
+          Contact Us
         </div>
-        {inProp1 && (
-          <div
-            className={`w-full px-2 flex justify-start items-start ${
-              toggle1
-                ? "fade-enter fade-enter-active"
-                : "fade-exit fade-exit-active"
-            }`}
-          >
-            <div className="px-6 border-l-[1px] border-[#dfdfdf] font-thin">
-              Let us know your specific requirements, and we'll match you with
-              the perfect plants.
-            </div>
-          </div>
-        )}
-        <div
-          onClick={handleToggle2}
-          className="w-full flex justify-start gap-4 items-start cursor-pointer"
-        >
-          <TbTriangleFilled
-            size={20}
-            className={`transform ${
-              toggle2 ? "rotate-180" : "rotate-90"
-            } duration-300`}
-          />
-          <h4>Enjoy expert guidance</h4>
-        </div>
-        {inProp2 && (
-          <div
-            className={`w-full px-2 flex justify-start items-start ${
-              toggle2
-                ? "fade-enter fade-enter-active"
-                : "fade-exit fade-exit-active"
-            }`}
-          >
-            <div className="px-6 border-l-[1px] border-[#dfdfdf] font-thin">
-              Benefit from our expert advice on plant care, maintenance, and
-              placement to ensure your plants thrive.
-            </div>
-          </div>
-        )}
       </div>
     </div>
   );
